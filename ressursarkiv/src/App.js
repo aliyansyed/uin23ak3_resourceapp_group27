65e22899add8f1edfcaae8d6c7ca7fbf3921808d
noomanraja <nooman9@hotmail.com>
2024-03-21 17:51:54 +0100

    updatta koden

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -8,24 +8,19 @@ import JavaScriptComp from './JavaScriptComp';
 import ReactComp from './ReactComp';
 import SanityComp from './SanityComp';
 
-
 function App() {
   return (
-    
-      <div>
-    <Routes>
-        <Route path="/html" element={HTMLComp} />
-        <Route path="/css" element={CSScomp} />
-        <Route path="/javascript" element={JavaScriptComp} />
-        <Route path="/react" element={ReactComp} />
-        <Route path="/sanity" element={SanityComp} />
-</Routes>
-      </div>
+    <div>
+      <Routes>
+        <Route path="/html" element={<HTMLComp />} />
+        <Route path="/css" element={<CSScomp />} />
+        <Route path="/javascript" element={<JavaScriptComp />} />
+        <Route path="/react" element={<ReactComp />} />
+        <Route path="/sanity" element={<SanityComp />} />
+      </Routes>
+    </div>
   );
 }
-
-
-
 
 export default App;
 
