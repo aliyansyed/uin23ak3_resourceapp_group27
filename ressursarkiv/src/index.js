d5cf2aeb865611c6f28453a4ad81dc51c046e070
yassin1256 <118130638+yassin1256@users.noreply.github.com>
2024-03-22 11:10:25 +0100

    endret css

--- ressursarkiv/src/index.js
+++ ressursarkiv/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App/>
+  </React.StrictMode>
+  );
