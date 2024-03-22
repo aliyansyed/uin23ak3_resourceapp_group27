5ec17ffba185e554c7cc60d707a8714e2f2f5c01
noomanraja <nooman9@hotmail.com>
2024-03-22 11:35:03 +0100

    d

--- ressursarkiv/src/index.js
+++ ressursarkiv/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App/>
-  </React.StrictMode>
-  );
+  </React.StrictMode>,
+  document.getElementById('root')
+);
